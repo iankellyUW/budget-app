bc1c07351489bb03f881182144968bafe48f7d27
Ian Kelly <ikelly@uwyo.edu>
2023-05-07 23:05:34 -0400

    Working transactions cookies

--- src/app/component/transactions-main/transactions-main.component.ts
+++ src/app/component/transactions-main/transactions-main.component.ts
@@ -1,8 +1,9 @@
-import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { MatTable } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Expenses } from 'src/app/model/expenses';
+import { CookieService } from 'ngx-cookie-service';
 
 export interface DialogData {
   animal: string;
@@ -31,10 +32,11 @@ const CATEGORY_DATA: Categories[] = []
   styleUrls: ['./transactions-main.component.scss']
 })
 
-export class TransactionsMainComponent implements OnInit {
+export class TransactionsMainComponent implements OnInit, AfterViewInit {
   @ViewChild(MatTable) table!: MatTable<Category>;
 
-  constructor(private router: Router, public dialog: MatDialog) { }
+  constructor(private router: Router, public dialog: MatDialog, private cookieService: CookieService,) {
+  }
 
   formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -42,7 +44,10 @@ export class TransactionsMainComponent implements OnInit {
   });
 
   ngOnInit(): void {
-    this.pushData();
+    //this.pushData();
+  }
+  ngAfterViewInit(): void {
+    this.readDataFromCookies();
   }
   //Default Values
   public currentDate = new Date();
@@ -60,16 +65,16 @@ export class TransactionsMainComponent implements OnInit {
 
 
   //category data object
-  categoryData: Categories = {date: '', amount: '$0.00', description: '',category: Category.empty };
+  categoryData: Categories = { date: '', amount: '$0.00', description: '', category: Category.empty };
 
-  addData() {
-    this.pushData();
-    this.table.renderRows();
-  }
 
   removeData() {
     this.dataSource.pop();
     this.table.renderRows();
+  }
+
+  setUserData() {
+    this.cookieService.set('user_transactions_data', JSON.stringify(this.dataSource));
   }
 
   openDialog(): void {
@@ -82,6 +87,7 @@ export class TransactionsMainComponent implements OnInit {
       this.categoryData = result;
       console.log(this.categoryData);
       this.pushNewExpense(this.categoryData);
+      this.setUserData();
     });
   }
 
@@ -101,12 +107,22 @@ export class TransactionsMainComponent implements OnInit {
     }
   }
 
-  pushData(): void {
-    this.dataSource.push({ date: (this.currentDate.getMonth()+1).toString()+'/'+this.currentDate.getDate().toString(), amount: this.formatter.format(100), description: 'I ordered food', category: Category.food },)
+  // pushData(): void {
+  //   this.dataSource.push({ date: (this.currentDate.getMonth() + 1).toString() + '/' + this.currentDate.getDate().toString(), amount: this.formatter.format(100), description: 'I ordered food', category: Category.food },);
+  // }
+
+  readDataFromCookies() {
+    if (this.cookieService.get('user_transactions_data') != '') {
+      let items = JSON.parse(this.cookieService.get('user_transactions_data')) as Categories[];
+      console.log(items);
+      for(let item of items) {
+        this.pushNewExpense(item);
+      }
+    }
   }
 
   pushNewExpense(category: Categories) {
-    this.dataSource.push({date: (this.currentDate.getMonth()+1).toString()+'/'+this.currentDate.getDate().toString(), amount: category.amount, description: category.description, category: category.category});
+    this.dataSource.push({ date: (this.currentDate.getMonth() + 1).toString() + '/' + this.currentDate.getDate().toString(), amount: category.amount, description: category.description, category: category.category });
     this.table.renderRows();
   }
 }
@@ -117,9 +133,10 @@ export class TransactionsMainComponent implements OnInit {
 })
 export class TransactionMainDialog {
   constructor(
+    private cookieService: CookieService,
     public dialogRef: MatDialogRef<TransactionMainDialog>,
     @Inject(MAT_DIALOG_DATA) public data: Categories,
-  ) {}
+  ) { }
 
   formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -129,13 +146,15 @@ export class TransactionMainDialog {
   //selected value for expense dialogue
   selectedValue: string = '';
 
-  categoryData: Categories = {date: '', amount: '$0.00', description: '',category: Category.empty };
+  categoryData: Categories = { date: '', amount: '$0.00', description: '', category: Category.empty };
 
   public expenses = new Expenses();
 
   onNoClick(): void {
     this.dialogRef.close();
   }
+
+
 
   confirmSelection(): void {
     this.categoryData.amount = this.formatter.format(Number(this.data.amount));
